55a194eb05d2fe75d39db6e8e412aec8ad420f40
Kgethego Masilo <kgethi47@gmail.com>
2025-04-21 22:56:19 +0200

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,8 @@ import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
     resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        const page = pages[`./Pages/${name}.vue`]
+        const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
+        const page = pages[`./pages/${name}.vue`]
 
         if (!page) {
             throw new Error(`Page not found: ./Pages/${name}.vue`)
