3151e6cccd2894465cdf001a20b05b709381a98a
Kgethego Masilo <kgethi47@gmail.com>
2025-04-21 22:49:41 +0200

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,8 @@ import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./Pages/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+      return pages[`./Pages/${name}.vue`]?.default
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
